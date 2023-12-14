1398f7960f566bf7e3b94ce3b7a0f81129579b85
Bhumzz Parate <bunnyparate7183@gmail.com>
2023-12-14 12:13:58 +0530

    working on design

--- resources/frontend/components/DesignSettings.jsx/DesignSettings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DesignSettings() {
-    return <div>DesignSettings</div>;
-}
-
-export default DesignSettings;
--- resources/frontend/components/SideBar.jsx
+++ resources/frontend/components/SideBar.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-import DesignSettings from "./DesignSettings.jsx/DesignSettings";
+import React, { useState } from "react";
+import DesignSettings from "./DesignSettings/DesignSettings";
 import GeneralSettings from "./GeneralSettings/GeneralSettings";
-import { Button, Card } from "@shopify/polaris";
+import { Badge, Button, Card } from "@shopify/polaris";
+import Switch from "react-switch";
+import "../css/index.css";
 
 export function SideBar() {
+    const [isChecked, setChecked] = useState(false);
+
+    const handleSwitchChange = (checked) => {
+        setChecked(checked);
+    };
     return (
         <div>
             <Card sectioned>
                 <div className="setting_title">
                     <span className="show_sticky_span">
-                        Add To Cart Sticky is{" "}
+                        Sticky Add To Cart is{" "}
+                        {isChecked ? (
+                            <span className="lm_sticky_custom_badge_success">
+                                <Badge tone="success">Enabled</Badge>
+                            </span>
+                        ) : (
+                            <span className="lm_sticky_custom_badge_critical">
+                                <Badge tone="critical">Disabled</Badge>
+                            </span>
+                        )}
                         {/* <b>{enable === true ? "Enabled" : "Disabled"}</b>{" "} */}
                     </span>
-                    {/* <div className="show_cart_btn"> */}
-                    <Button
-                        primary
-                        // onClick={() => {
-                        //     handleEnable(enable);
-                        // }}
-                    >
-                        {/* {enable === true ? "Disable" : "Enable"} */}
-                    </Button>
+                    <Switch
+                        onChange={handleSwitchChange}
+                        checked={isChecked}
+                        uncheckedIcon={null}
+                        checkedIcon={null}
+                    />
                 </div>
             </Card>
             <Card sectioned>
