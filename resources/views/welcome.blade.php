07715b857227f7a4cbeaa78538cfe74d01c7c295
Vidhee <vidhee.luckimedia@gmail.com>
2025-01-10 16:53:43 +0530

    clarity script added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,24 +11,41 @@
 
 <body>
     <div id="app"></div>
-    <input type="hidden" id="apiKey" value={{env('VITE_SHOPIFY_API_KEY')}}>
-    <input type="hidden" id="shopOrigin" value="<?php  echo $_GET['shop']; ?>">
+    <input type="hidden" id="apiKey" value={{ env('VITE_SHOPIFY_API_KEY') }}>
+    <input type="hidden" id="shopOrigin" value="<?php echo $_GET['shop']; ?>">
     {{-- <input type="hidden" id="shopOrigin" value="{{ $shopDomain ?? Auth::user()->name }}"> --}}
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/64b50f1c94cf5d49dc640de0/1h5hk62p5';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/64b50f1c94cf5d49dc640de0/1h5hk62p5';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
     </script>
     <!--End of Tawk.to Script-->
+
+    <!-- Starting of MS Clarity Script -->
+    <script type="text/javascript">
+        (function(c, l, a, r, i, t, y) {
+            c[a] =
+                c[a] ||
+                function() {
+                    (c[a].q = c[a].q || []).push(arguments);
+                };
+            t = l.createElement(r);
+            t.async = 1;
+            t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "pro7d4lwyn");
+    </script>
+    <!-- Ending of MS Clarity Script -->
 </body>
 
 </html>
